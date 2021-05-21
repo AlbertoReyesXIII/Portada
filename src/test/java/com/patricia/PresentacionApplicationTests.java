7e922c61ae391f7031f7ec21e7bd63ff684b9944
paty6 <paty6@LAPTOP-PQNMHTVG>
2021-05-21 15:18:34 -0500

    Principal

---
+++ src/test/java/com/patricia/PresentacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patricia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresentacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
